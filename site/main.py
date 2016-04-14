0b2fcc3bccfdd9aad6cdec6cb96308ce9d93b6c2
shuhaokou <shuhaokou@gmail.com>
2016-04-14 14:53:56 -0500

    Revert "Removed site stuff from mobile"
    This reverts commit 8982738d921f8da2c5f834e6af8e0996791b3551.

---
+++ site/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, url_for, render_template
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/index")
+@app.route("/")
+def display_index():
+    return render_template("index.html", page_title="Lorem")
+
+@app.route("/login")
+def display_login():
+    return render_template("login.html", page_title="Login")
+
+@app.route("/register")
+def display_register():
+    return render_template("register.html", page_title="Register")
+
+@app.route("/donations")
+def display_donations():
+    return render_template("donations.html", page_title="Donations")
+
+if __name__ == "__main__":
+    app.run()
