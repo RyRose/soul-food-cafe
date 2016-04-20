268dd2699bc56128b74c117f3ccad7ff9c5c85c1
Ryan Rose <ryanthrose@gmail.com>
2016-04-19 22:56:13 -0500

    fancy jquery donation table

--- site/main.py
+++ site/main.py
@@ -17,20 +17,29 @@ def login():
         return render_template("login.html", page_title="Login")
 
     elif request.method == "POST":
-        session['username'] = request.form['username']
-        return redirect(url_for('donations'))
+        # TODO: Check with database if user is in it.
+        username = request.form['username']
+
+        if (username, request.form['password']) == ('admin', 'admin'):
+            return redirect(url_for('donations'))
+        else:
+            return render_template("login.html", page_title="Login", invalid=True)
     else:
         return render_template("login.html", page_title="Login")
 
 @app.route("/register", methods = ['GET', 'POST'])
 def register():
     if request.method == "POST":
+        # TODO: Add donator to database. Possibly setup email confirmation.
         return redirect(url_for('login'))
     else:
         return render_template("register.html", page_title="Register")
 
 @app.route("/donations")
 def donations():
+    # TODO: Add check for username in session
+
+
     return render_template("donations.html", page_title="Donations")
 
 if __name__ == "__main__":
